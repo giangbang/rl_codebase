844f3e1128a8a3f2c0becd54311dc804bd1326d4
giangbang <banggiangle2015@gmail.com>
2022-09-27 13:24:43 +0700

    add `DummyVecEnv` for debugging `gym.vector.VectorEnv`

--- rl_codebase/core/vec_env.py
+++ rl_codebase/core/vec_env.py
@@ -1,7 +1,47 @@
 import gym
+import numpy as np
 from gym.vector import SyncVectorEnv
 from rl_codebase.core.utils import get_obs_shape, get_action_dim
 
+
+class DummyVecEnv(SyncVectorEnv):
+    def reset(self):
+        obs_shape = self.single_observation_space.shape
+        states = []
+        for env in self.envs:
+            states.append(env.reset())
+        return np.array(states).reshape(self.num_envs, *obs_shape)
+
+    def step(self, actions):
+        obs_shape = self.single_observation_space.shape
+        infos_key = set()
+        next_states, rewards, dones, infos = [], [], [], []
+        for i, (env , action) in enumerate(self.envs, actions):
+            next_state, reward, done, info = env.step(action)
+            
+            if done:
+                info.update({'final_observation': next_state})
+                next_state = env.reset()
+            
+            next_states.append(next_state)
+            rewards.append(reward)
+            dones.append(done)
+            infos.append(info)
+            
+            infos_key.update(info.keys())
+        
+        info_return = {}
+        for key in infos_key:
+            val = []
+            for inf in infos:
+                val.append(inf.get(key, None))
+            info_return[key] = np.array(val)
+            
+        return np.array(next_states).reshape(self.num_envs, *obs_shape),
+               np.array(rewards).reshape(self.num_envs, -1),
+               np.array(dones).reshape(self.num_envs, -1),
+               info_return
+        
 
 class VecEnv(SyncVectorEnv):
     """
